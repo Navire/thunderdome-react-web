4883950e57a08af622b16c5671460bf2671e9f1b
Erivan Souza da Silva Filho <erivan.filho@citsamazonas.local>
2019-11-19 18:00:07 -0400

    first structure

---
+++ src/Common/constants.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const menuItems = [
+    "JOGAR",
+    "OPÇÕES",
+    "SAIR",
+];
+
---
+++ src/Components/Menu/Menu.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import menuItems from '../../Common/constants';
+
+class Menu extends Component {
+    render() {
+        return (
+          <div>
+              here
+          </div>
+        )
+    }   
+}
+
+export default Menu;
